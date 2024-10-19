f3259b094b0ad37079782668e1010f88fd8fa35a
ArTeGaS-2 <probablytorvik@gmail.com>
2024-10-19 20:00:28 +0300

    spawner done

--- Assets/MicroOrg/Spawner.cs
+++ Assets/MicroOrg/Spawner.cs
@@ -7,10 +7,13 @@ public class Spawner : MonoBehaviour
     // Список з gameObject(ігрові об'єкти) для спавну
     public List<GameObject> organisms;
 
+    // Максимальне число сворених об'єктів
     public int spawnNumber = 10;
 
+    // Діаметр, в якому відбудется спавн
     public float diameter = 5f;
 
+    // Кординати, що змінюються після кожного спавну
     private float x_Pos;
     private float z_Pos;
     private void Start()
@@ -20,8 +23,19 @@ public class Spawner : MonoBehaviour
         // "i" збільшується на 1 кожну ітерацію циклу
         for (int i = 0; i < spawnNumber; i++)
         {
-            x_Pos = Random.Range(0f, diameter);
-            z_Pos = Random.Range(0f, diameter);
+            // Даємо випадкові кординати
+            x_Pos = Random.Range(-diameter, diameter);
+            z_Pos = Random.Range(-diameter,diameter);
+            // Обираємо конкретний організм зі списку
+            GameObject organism = organisms[Random.Range(0, organisms.Count -1)];
+            // Визначаємо точку спавну
+            Vector3 spawnPos = new Vector3(
+                x_Pos,
+                organism.transform.position.y,
+                z_Pos);
+            // Строємо об'єкт на сцені
+            Instantiate(organism, spawnPos, organism.transform.rotation);
         }
+        
     }
 }
