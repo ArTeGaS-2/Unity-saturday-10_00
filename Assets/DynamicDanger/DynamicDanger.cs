2223a53d2d06eecaeecf3055e7ed5c54dd885f1b
ArTeGaS-2 <probablytorvik@gmail.com>
2024-12-21 12:58:37 +0200

    Full static danger and dynamic danger script

---
+++ Assets/DynamicDanger/DynamicDanger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicDanger : MonoBehaviour
+{
+    public float needToGo = 3f; // Затримка до наступної дії
+    public float objectSpeed = 5f; // Швидкість об'єкту
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
