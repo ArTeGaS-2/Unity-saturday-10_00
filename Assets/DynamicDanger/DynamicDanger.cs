484731d2b606e0adf45afe9c03e6e1df43bda29e
ArTeGaS-2 <probablytorvik@gmail.com>
2024-12-28 11:57:43 +0200

    Перероблена структура компонентів DangerObject та додано нову логіку у скрипт

--- Assets/DynamicDanger/DynamicDanger.cs
+++ Assets/DynamicDanger/DynamicDanger.cs
@@ -6,11 +6,21 @@ public class DynamicDanger : MonoBehaviour
 {
     public float needToGo = 3f; // Затримка до наступної дії
     public float objectSpeed = 5f; // Швидкість об'єкту
-    private void OnCollisionEnter(Collision collision)
+
+    private Rigidbody rb; // Змінна для компоненту Rigidbody
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            Destroy(collision.gameObject);
+            Destroy(other.gameObject);
         }
+    }
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>(); // 
+    }
+    private void Update()
+    {
+        rb.velocity = Vector3.forward * objectSpeed * Time.deltaTime;
     }
 }
