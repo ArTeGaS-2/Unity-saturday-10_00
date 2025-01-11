b373538197b5855369bee55de6f9578d7454c84f
ArTeGaS-2 <probablytorvik@gmail.com>
2025-01-11 13:55:38 +0200

    Частково додано механіку обертання через корутіни, виправлено деякі помилки

--- Assets/DynamicDanger/DynamicDanger.cs
+++ Assets/DynamicDanger/DynamicDanger.cs
@@ -8,6 +8,9 @@ public class DynamicDanger : MonoBehaviour
     public float objectSpeed = 5f; // Швидкість об'єкту
 
     private Rigidbody rb; // Змінна для компоненту Rigidbody
+
+    private float currentAngleMod = 1f;
+    public float anglePerIteration = 90f; // Те наскільки міняється кут за раз
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -17,10 +20,22 @@ public class DynamicDanger : MonoBehaviour
     }
     private void Start()
     {
-        rb = GetComponent<Rigidbody>(); // 
+        rb = GetComponent<Rigidbody>(); // Привязка фізичного компонента rb
+        StartCoroutine(RotateDynamicDanger());
     }
     private void Update()
     {
-        rb.velocity = Vector3.forward * objectSpeed * Time.deltaTime;
+        rb.velocity = Vector3.left * objectSpeed * Time.deltaTime;
+    }
+    private IEnumerator RotateDynamicDanger()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(needToGo);
+            transform.rotation = Quaternion.Euler(
+                transform.rotation.x,
+                transform.rotation.y,
+                transform.rotation.z + anglePerIteration); // + кут за ітерацію
+        }
     }
 }
