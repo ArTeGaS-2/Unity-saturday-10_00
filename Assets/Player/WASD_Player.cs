cc9afe520627943cbc09d54b185351b9951e4e32
ArTeGaS-2 <probablytorvik@gmail.com>
2025-03-15 16:04:24 +0200

    Моделі, анімації та оновлення скрипта гравця

---
+++ Assets/Player/WASD_Player.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD_Player : MonoBehaviour
+{
+    public static WASD_Player Instance; // Паттерн Сінглтон
+
+    public Camera mainCamera; // Посилання на головну Камеру
+    private static float cameraDistance = 7f; // Відстань від персонажа
+
+    public float maxSpeed = 10f; // Максимальна швидкість
+    public float rotationSpeed = 10f; // Швидкість обертання
+
+    private Rigidbody rb; // Посилання на фізичний компонент
+    public Animator animator; // Посилання на компонент Аніматору
+
+    private void Start()
+    {
+        Instance = this; // Бере з пам'яті екземпляр скрипта
+        rb = GetComponent<Rigidbody>(); // Призначення компонента в змінну
+    }
+    private void Update()
+    {
+        // Отримуємо значення вводу з клавіатури
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0,
+            Input.GetAxis("Vertical"));
+        // Пряме присвоєння швидкості (замість AddForce)
+        if(moveInput.magnitude > 0.1f)
+        {
+            Vector3 desiredVelocity = moveInput.normalized * maxSpeed;
+            rb.velocity = new Vector3(desiredVelocity.x,
+                rb.velocity.y, desiredVelocity.z);
+        }
+        else
+        {
+            rb.velocity = new Vector3 (0, rb.velocity.y, 0);
+        }
+        // Обчислюємо локальний вектор руху відносно поточного повороту
+        // моделі
+        Vector3 localMovement = transform.InverseTransformDirection(
+           moveInput);
+        // Передаємо локальні координати в аніматор
+        animator.SetFloat("VelocityX", localMovement.x);
+        animator.SetFloat("VelocityZ", localMovement.z);
+
+        // Обертання моделі у напрямку миші
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, transform.position);
+    }
+}
